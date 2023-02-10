36290181dc67b58cf9c9d7b57a0b0e89b6483b8f
dominictrinh <dominictrinh0@gmail.com>
2023-02-10 13:09:13 -0500

    Fixed the fov

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -14,8 +14,22 @@ public class FieldOfView : MonoBehaviour {
     [HideInInspector]
     public List<Transform> visibleTargets = new List<Transform>();
 
-    void Start() {
+    public float meshResolution;
+    public MeshFilter viewMeshFilter;
+    Mesh viewMesh;
+    
+    void Start()
+    {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
+        
         StartCoroutine ("FindTargetsWithDelay", .2f);
+    }
+
+    void LateUpdate()
+    {
+        DrawFieldOfView();
     }
 
 
@@ -25,29 +39,91 @@ public class FieldOfView : MonoBehaviour {
             FindVisibleTargets ();
         }
     }
-
+    
     void FindVisibleTargets() {
         visibleTargets.Clear ();
         Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
-
+    
         for (int i = 0; i < targetsInViewRadius.Length; i++) {
             Transform target = targetsInViewRadius [i].transform;
-            Vector3 dirToTarget = (target.position - transform.position).normalized;
-            if (Vector3.Angle (transform.forward, dirToTarget) < viewAngle / 2) {
-                float dstToTarget = Vector3.Distance (transform.position, target.position);
-
-                if (!Physics.Raycast (transform.position, dirToTarget, dstToTarget, obstacleMask)) {
+            Vector2 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector2.Angle (transform.up, dirToTarget) < viewAngle / 2) {
+                float dstToTarget = Vector2.Distance (transform.position, target.position);
+    
+                if (!Physics2D.Raycast (transform.position, dirToTarget, dstToTarget, obstacleMask)) {
                     visibleTargets.Add (target);
                 }
             }
         }
     }
 
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = -transform.eulerAngles.z - viewAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            viewPoints.Add(newViewCast.point);
+        }
 
-    public Vector2 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+        viewMesh.Clear ();
+
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals ();
+    }
+
+    ViewCastInfo ViewCast(float globalAngle) {
+        Vector3 dir = DirFromAngle (globalAngle, true);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(dir.x, dir.y), viewRadius, obstacleMask);
+    
+        if (hit != null && hit.collider != null) {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        } else {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+        }
+    }
+    
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
         if (!angleIsGlobal) {
             angleInDegrees -= transform.eulerAngles.z;
         }
-        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), 0);
     }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dst;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            dst = _dst;
+            angle = _angle;
+        }
+    }
+
 }
