72e0f78f3049b68aeb4e811a8e0ac76ab614171b
agent30632 <49417970+agent30632@users.noreply.github.com>
2023-03-06 17:08:10 -0500

    Basic main menu
    Added a basic main menu that currently loads into the video demo level

---
+++ Assets/Scripts/UI/SwitchScene.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public string sceneName;
+    public void OnButtonPress()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
