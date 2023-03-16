7111caff825ca6a6e7769de6f7ede3c25b1be755
stevenvan0 <stevenlv.van@gmail.com>
2023-03-16 18:08:17 -0400

    Added sliding door, toggle door, click/hover trigger

--- Assets/Scripts/Door/DoorAnimator.cs
+++ Assets/Scripts/Door/DoorAnimator.cs
@@ -1,23 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Tilemaps;
 
 public class DoorAnimator : MonoBehaviour
 {
-
+    [SerializeField] private GameObject closedShadow;
+    [SerializeField] private Tilemap door1;
+    [SerializeField] private Tilemap door2;
+    
     private Animator animator;
+    private ShadowCaster2D shadow1;
+    private ShadowCaster2D shadow2;
     
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        shadow1 = door1.GetComponent<ShadowCaster2D>();
+        shadow2 = door2.GetComponent<ShadowCaster2D>();
     }
     public void OpenDoor()
     {
         animator.SetBool("Open", true);
+        closedShadow.SetActive(false);
+        shadow1.enabled = true;
+        shadow2.enabled = true;
     }
 
-    public void CloseDoor()
+    public IEnumerator CloseDoor()
     {
         animator.SetBool("Open", false);
+        yield return new WaitForSeconds (2.5f);
+        closedShadow.SetActive(true);
+        shadow1.enabled = false;
+        shadow2.enabled = false;
     }
 }
