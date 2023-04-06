f8d720e7a1e2bef689b9fa19a7fe79f432ffb357
stevenvan0 <stevenlv.van@gmail.com>
2023-04-06 00:26:47 -0400

    help me

--- Assets/Scripts/Door/OnHover.cs
+++ Assets/Scripts/Door/OnHover.cs
@@ -2,44 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class OnHover : MonoBehaviour
 {
     [SerializeField] private DoorAnimator door;
     [SerializeField] private DoorToggle lvl2door;
-    [SerializeField] private ScannerPlayerBounds boundsScript;
 
-    private bool isMouseOver;
-    private bool isPlayerOver;
+    bool isHovering;
     public float timeToWait = 3f;
     float timeLeft;
-
     private void Awake()
     {
         timeLeft = timeToWait;
     }
-
     void OnMouseOver()
     {
-        isMouseOver = true;
+        isHovering = true;
     }
      
     void OnMouseExit()
     {
-        isMouseOver = false;
+        isHovering = false;
         timeLeft = timeToWait;
         lvl2door.CloseDoor();
     }
 
     void Update()
     {
-        if (!boundsScript.isPlayerOver)
-        {
-            timeLeft = timeToWait;
-            lvl2door.CloseDoor();
-        }
-
-        if (isMouseOver && boundsScript.isPlayerOver)
+        if (isHovering)
         {
             timeLeft-=Time.deltaTime;
         }
